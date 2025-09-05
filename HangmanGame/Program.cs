b676bac1c3cf38fe7ad513865a8de28c5d2dea15
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-04 19:34:01 -0500

    adding the first steps to my code. Creating a list. then pulling a random element from list using Random.next or a variation of it

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Let's play the hangman game!");
+        
+        List<string> possibleWords = new List<string>(){"galaxy", "asgard", "space", "flare"};
+
+        Random Word = new Random();
+        string randomWord = possibleWords[Word.Next(0, possibleWords.Count)];
+     
+        
     }
 }
