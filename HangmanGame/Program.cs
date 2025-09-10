517901d06f69c5667617fa80281a69ef08fd69a4
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-10 09:57:53 -0500

    getting confused. thinking about scraping some code and restarting.

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -10,13 +10,13 @@ class Program
 
         Random Word = new Random();
         string randomWord = possibleWords[Word.Next(0, possibleWords.Count)];
-        string charInput = Console.WriteLine("What's your letter choice: ");
+        /*var charInput = Console.WriteLine("What's your letter choice: ");
         
         if (charInput == char.IsLetter(randomWord[0]))
         {
         // how to not be redundant with looking for letters in each character spot?
             Console.WriteLine(charInput);
-        }   
+        }   */
         
             
     }
