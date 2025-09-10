dc3b09f6b4ca6858c89daaa22f85718165d23f93
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-10 14:23:07 -0500

    foreach for a character in randomWord

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -8,15 +8,26 @@ class Program
         
         List<string> possibleWords = new List<string>(){"galaxy", "asgard", "space", "flare"};
 
-        Random Word = new Random();
-        string randomWord = possibleWords[Word.Next(0, possibleWords.Count)];
+        Random word = new Random();
+        string randomWord = possibleWords[word.Next(0, possibleWords.Count)];
         
         Console.WriteLine("What's your letter choice: ");
         string charInput = Console.ReadLine();
+        foreach (char c in randomWord)
+        {
+            if (charInput == )
+            {
+                Console.WriteLine(c);
+            }   
+        }
         
+       
+        
+        //should I put the chosen random word into a variable while cycling through the letters?
         //place randomWord into a function (or is that how one would in JS) that cycles through the character array of that
         //particular randomWord. if yes - print letter in correct position and ask for another letter. if no state "try again".
         //print out the current state of the game.
+        
         
         //
         
