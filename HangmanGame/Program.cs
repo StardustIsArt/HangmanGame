4a48b23813c806293748f8a000ed12db44c1a189
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-10 10:23:53 -0500

    deleting bad code. asking letter choice - assigning that to a string var.
    also writing my thought process of what needs to be accomplished in comments for myself.

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -10,13 +10,16 @@ class Program
 
         Random Word = new Random();
         string randomWord = possibleWords[Word.Next(0, possibleWords.Count)];
-        /*var charInput = Console.WriteLine("What's your letter choice: ");
         
-        if (charInput == char.IsLetter(randomWord[0]))
-        {
-        // how to not be redundant with looking for letters in each character spot?
-            Console.WriteLine(charInput);
-        }   */
+        Console.WriteLine("What's your letter choice: ");
+        string charInput = Console.ReadLine();
+        
+        //place randomWord into a function (or is that how one would in JS) that cycles through the character array of that
+        //particular randomWord. if yes - print letter in correct position and ask for another letter. if no state "try again".
+        //print out the current state of the game.
+        
+        //
+        
         
             
     }
