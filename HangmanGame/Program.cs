bae15c72f7e1b11089a3e7a62cc97de52048e5c8
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-20 12:23:07 -0500

    getting value of input and checking that against randomWord to show the index of the inputed letter

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -9,18 +9,22 @@ class Program
         Console.WriteLine("Let's play the hangman game!");
         
         List<string> possibleWords = new List<string>(){"galaxy", "asgard", "space", "flare", "quantum"};
-
+        const string MISSING_VALUE = "_";
         Random rand = new Random();
         string randomWord = possibleWords[rand.Next(0, possibleWords.Count)];
-   
+        Console.WriteLine(randomWord);
         Console.WriteLine("What's your letter choice: ");
         string charInput = Console.ReadLine();
 
         char value = char.Parse(charInput);
-        string missingValue = "_";
-        if (value != char.Parse(randomWord))
+        
+        Console.WriteLine(randomWord.IndexOf(value));
+            
+        
+       /* if (value != char.Parse(randomWord))
         {
             Console.WriteLine(missingValue);
-        }
+        }*/
+       
     }
 }
