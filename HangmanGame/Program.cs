9134b6108bf9ac094cd846e52ae99cc3c08b9c2c
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-28 16:09:30 -0500

    rearranged some code to read better.

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -11,23 +11,24 @@ class Program
         List<string> possibleWords = new List<string>(){"galaxy", "asgard", "space", "flare", "quantum"};
         Random rand = new Random();
         string randomWord = possibleWords[rand.Next(0, possibleWords.Count)];
+        foreach (char c in randomWord)
+        {
+            Console.Write("_ ");
+        }
+        Console.WriteLine("\nWhat's your letter choice: ");
         
-        Console.WriteLine("What's your letter choice: ");
         string charInput = Console.ReadLine();
-
         char value = char.Parse(charInput);
 
         if (randomWord.Contains(value))
         {
-            Console.WriteLine(randomWord.IndexOf(value));
-
             foreach (char c in randomWord)
             {
                 if (Char.IsLetter(c))
-                    Console.Write("_ ");
+                    Console.Write(randomWord.IndexOf(value));
                 else
                 {
-                    Console.Write(c + " ");
+                    Console.Write("_ ");
                 }
             }
         }
